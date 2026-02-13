505dafe30b0de55f26551bb02aac2314be76bc65
Armin <armin.hamedazimi@cafebazaar.ir>
2026-02-13 22:07:17 +0330

    Move Celery configuration from app404 to team2 module and update Celery app name and autodiscovery settings

--- team2/celery.py
+++ team2/celery.py
@@ -3,9 +3,9 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app404.settings')
 
-app = Celery('app404')
+app = Celery('team2')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+app.autodiscover_tasks(['team2.tasks'])
 app.conf.include = [
     'team2.tasks.tasks',
     'team2.tasks.indexing',
